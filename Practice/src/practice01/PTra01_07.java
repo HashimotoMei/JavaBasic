de5b4f7897d189d038b9ca384dfadf667e1e766b
橋本めい <hashimoto111mei@gmail.com>
2017-12-18 18:25:18 +0900

    4まで完了

---
+++ Practice/src/practice01/PTra01_07.java
@@ -0,0 +1,44 @@
+package practice01;
+/*
+ * PTra01_07.java
+ *   作成	LIKEIT	2017
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+public class PTra01_07 {
+	public static void main(String[] args) {
+
+		// 基本型8種類の変数を宣言します
+		boolean bo;
+		byte by;
+		short s;
+		char c;
+		int i;
+		long l;
+		float f;
+		double d;
+
+		// ★ それぞれの型変数に、値を代入してください
+		// ※ 値は、好きな数字を入力してください
+		bo = true;
+		by = 10;
+		s = 5;
+		c = 'A';
+		i = 150;
+		l = 12345L;
+		f = 1.5F;
+		d = 2.5;
+
+		// ★ それぞれの変数の中身を出力してください
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+
+	}
+}
